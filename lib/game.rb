d902d5c0b6d959569b4554770cdeffcdbd8fe5a3
Albalenys <albalenysd@gmail.com>
2015-11-02 18:37:29 -0500

    improve game instructions

--- lib/game.rb
+++ lib/game.rb
@@ -10,17 +10,19 @@ class Game
   def start_game
     system("clear")
     puts "Welcome to Tic Tac Toe!"
+    puts "First player to get three in a row wins."
     puts "Choose to play as either 'X' or 'O'."
     choose_marker
+    system("clear")
     puts "Please select your spot."
     puts "\n"
     puts self
     until game_is_over(@board) || tie(@board)
       get_human_spot
       system("clear")
-      puts "You(#{@hum}) moved to position #{@current_spot}."
+      puts "You (#{@hum}) moved to position #{@current_spot}."
       get_comp_spot
-      puts "Computer(#{@com}) moved to position #{@current_spot}."
+      puts "#{@com} moved to position #{@current_spot}."
       puts "\n"
       puts self
     end
@@ -31,12 +33,10 @@ class Game
 
   def choose_marker
     @hum = gets.chomp.upcase!
-
     until @hum == "X" || @hum == "O"
       puts "Please select to play as either 'X' or 'O'."
       @hum = gets.chomp.upcase!
     end
-
     if @hum == 'X'
       @com = 'O'
     else
