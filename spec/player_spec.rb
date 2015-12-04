f02f609662cc3025d16ec5890be0d9c8445d0c3d
Albalenys <albalenysd@gmail.com>
2015-12-04 03:15:50 -0500

    expanded tests for player#get_move

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -23,46 +23,64 @@ describe 'Player' do
   end
 
   describe '#get_move' do
-    it 'computers choose middle spot when available' do
-      @board.spots = Array(1..9)
-      expect{@computer.get_move(@board)}.to change{@board.spots[4]}.to('O')
-      count = @board.spots.select { |spot| spot == 'O'}
-      expect(count.length).to eq(1)
+    context 'when safe from losing' do
+      it 'computers choose middle spot when available' do
+        @board.spots = Array(1..9)
+        expect{@computer.get_move(@board)}.to change{@board.spots[4]}.to('O')
+        count = @board.spots.select { |spot| spot == 'O'}
+        expect(count.length).to eq(1)
+      end
+
+      it 'computers choose spots that will give diagonal win' do
+        @board.spots = ['O', 'X', 3, 4, 'O', 6, 'X', 8, 9]
+        expect{@computer.get_move(@board)}.to change{@board.spots[8]}.to('O')
+        count = @board.spots.select { |spot| spot == 'O'}
+        expect(count.length).to eq(3)
+      end
+
+      it 'computers choose spots that will give horizontal win' do
+        @board.spots = [1, 'X', 3, 'O', 'O', 6, 'X', 8, 9]
+        expect{@computer.get_move(@board)}.to change{@board.spots[5]}.to('O')
+        count = @board.spots.select { |spot| spot == 'O'}
+        expect(count.length).to eq(3)
+      end
+
+      it 'computers choose spots that will give vertical win' do
+        @board.spots = ['O', 2, 'X', 'O', 5, 6, 7, 'X', 9]
+        expect{@computer.get_move(@board)}.to change{@board.spots[6]}.to('O')
+        count = @board.spots.select { |spot| spot == 'O'}
+        expect(count.length).to eq(3)
+      end
+
+      it 'computers choose randomly when there is no best move' do
+        @board.spots = ['O', 2, 3, 4, 'X', 6, 7, 8, 9]
+        @computer.get_move(@board)
+        count = @board.spots.select { |spot| spot == 'O'}
+        expect(count.length).to eq(2)
+      end
     end
 
-    it 'computers choose spots that will give diagonal win' do
-      @board.spots = ['O', 'X', 3, 4, 'O', 6, 'X', 8, 9]
-      expect{@computer.get_move(@board)}.to change{@board.spots[8]}.to('O')
-      count = @board.spots.select { |spot| spot == 'O'}
-      expect(count.length).to eq(3)
-    end
+    context 'when endanger of losing' do
+      it 'computers avoid losing even with middle spot available' do
+        @board.spots = ['O', 2, 'X', 4, 5, 'X', 7, 8, 9]
+        expect{@computer.get_move(@board)}.to change{@board.spots[8]}.to('O')
+        count = @board.spots.select { |spot| spot == 'O'}
+        expect(count.length).to eq(2)
+      end
 
-    it 'computers choose spots that will give horizontal win' do
-      @board.spots = [1, 'X', 3, 'O', 'O', 6, 'X', 8, 9]
-      expect{@computer.get_move(@board)}.to change{@board.spots[5]}.to('O')
-      count = @board.spots.select { |spot| spot == 'O'}
-      expect(count.length).to eq(3)
-    end
+      it 'computers avoid losing even with possible horizontal win' do
+        @board.spots = ['X', 'X', 3, 'O', 'O', 6, 7, 8, 9]
+        expect{@computer.get_move(@board)}.to change{@board.spots[2]}.to('O')
+        count = @board.spots.select { |spot| spot == 'O'}
+        expect(count.length).to eq(3)
+      end
 
-    it 'computers choose spots that will give vertical win' do
-      @board.spots = ['O', 2, 'X', 'O', 5, 6, 7, 'X', 9]
-      expect{@computer.get_move(@board)}.to change{@board.spots[6]}.to('O')
-      count = @board.spots.select { |spot| spot == 'O'}
-      expect(count.length).to eq(3)
-    end
-
-    it 'computers choose spots that avoid lost' do
-      @board.spots = ['X', 2, 'O', 'X', 5, 'O', 7, 8, 9]
-      expect{@computer.get_move(@board).to change(@board.spots[6].to ('O'))}
-      # count = @board.spots.select { |spot| spot == 'O'}
-      # expect(count.length).to eq(3)
-    end
-
-    it 'computers choose randomly when there is no best move' do
-      @board.spots = ['O', 2, 3, 4, 'X', 6, 7, 8, 9]
-      @computer.get_move(@board)
-      count = @board.spots.select { |spot| spot == 'O'}
-      expect(count.length).to eq(2)
+      it 'computers avoid losing even with possible vertical win' do
+        @board.spots = ['O', 'X', 3, 'O', 'X', 6, 7, 8, 9]
+        expect{@computer.get_move(@board)}.to change{@board.spots[7]}.to('O')
+        count = @board.spots.select { |spot| spot == 'O'}
+        expect(count.length).to eq(3)
+      end
     end
   end
 end
